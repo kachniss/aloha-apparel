31790fd4ecc4f637912b2003f547cab68c10a394
Kachniss <vopalkova.katerina@gmail.com>
2018-03-26 20:42:03 -0700

    Add skip link, tota11y.js

--- js/index.js
+++ js/index.js
@@ -76,4 +76,13 @@ $(function(){
         imagesLoaded: true,
         wrapAround: true
     });
+
+    //
+    // Fix "Skip Link" Focus in Webkit
+    //
+    $(function() {
+        $("a[href^='#']").not("a[href='#']").click(function() {
+        $("#"+$(this).attr("href").slice(1)+"").focus();
+        });
+    });
 });
